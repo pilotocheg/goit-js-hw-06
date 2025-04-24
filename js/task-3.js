3744ac8fa09c5819548c1876781ff5403735d6dc
Viacheslav Dmytriiev <v.dmytriiev@sdventures.com>
2025-04-24 18:32:18 +0200

    Added home work files

---
+++ js/task-3.js
@@ -0,0 +1,30 @@
+class StringBuilder {
+  constructor(initialValue) {
+    this.value = initialValue;
+  }
+
+  padStart(str) {
+    this.value = str + this.value;
+  }
+
+  padEnd(str) {
+    this.value += str;
+  }
+
+  padBoth(str) {
+    this.value = str + this.value + str;
+  }
+
+  getValue() {
+    return this.value;
+  }
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
